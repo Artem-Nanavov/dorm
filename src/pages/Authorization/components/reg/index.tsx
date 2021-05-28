53a37820a14f16cb9e436e0ba0daf7b5412c4a06
Lisa <elizaveta.okorakova.anatolyevna@gmail.com>
2021-05-28 23:27:10 +0300

    Сделан скелет регистрации

--- src/pages/Authorization/components/reg/index.tsx
+++ src/pages/Authorization/components/reg/index.tsx
@@ -1,46 +1,14 @@
-import React, {useState, useEffect, useCallback} from 'react';
-import './style.css';
+import React from 'react';
+import '/'
 
-
-interface Reg {
-	loginReqSaga: (pws: string, email: string) => void,
-	regReqSaga: (pws: string, email: string, username: string) => void,
-}
-
-const Form = ({
-	loginReqSaga,
-	regReqSaga,
-}: Reg) => {
-	const [isReg, setIsReg] = useState(true);
-
-	const [email, setEmail] = useState('');
-	const [pws, setPws] = useState('');
-	const [firstName, setFirstName] = useState('');
-	const [lastName, setLastName] = useState('');
-	const [isDisabledBtn, setIsDisabledBtn] = useState(true);
-
-	useEffect(() => {
-		setIsDisabledBtn(pws.trim().length < 6 || email.trim().length < 5);
-	}, [pws, email]);
-
-	const loginHandler = useCallback(() => {
-		if (isDisabledBtn) return;
-
-		try {
-			if (!isReg) { regReqSaga(email, pws, `${firstName} ${lastName}`);
-			} else {
-				loginReqSaga(email, pws);
-			}
-		} catch (e) {
-			console.log('e', e.message);
-
-			alert('Error');
-		}
-	}, [email, pws, isDisabledBtn, firstName, lastName, isReg]);
-
-	return (
-
-			<div className={styles.loginForm__fields}>
-				{
-					
-export default Reg;
+function App() {
+    return (
+        <div className='app'>
+            <header className='header'>
+                <div className='container'>
+                    <div className='header-reg'>Регистрация</div>
+                </div>
+            </header>
+        </div>
+        )
+    }
