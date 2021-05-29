ff94b91221d548788f242eaf946f0e4e0d3e39b5
artem_nanavov <artemnanavov@gmail.com>
2021-05-29 10:15:10 +0300

    restore eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,7 +36,7 @@ module.exports = {
 		'eslintjsx-a11y/alt-text': 'off',
 		'react/prop-types': 'off',
 		camelcase: 'off',
-		indent: 'off',
+		indent: [2, 'tab'],
 		'react/jsx-indent': [2, 'tab'],
 		'react/jsx-indent-props': [2, 'tab'],
 		'no-tabs': 'off',
--- src/pages/Authorization/components/reg/index.tsx
+++ src/pages/Authorization/components/reg/index.tsx
@@ -32,5 +32,5 @@ const Reg = () => (
 			</div>
 		</div>
 	</div>
-        );
+);
 export default Reg;
