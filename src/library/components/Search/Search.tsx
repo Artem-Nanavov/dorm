fd71d0ed4770270a0a081a88c874597ba4ace8ac
DmitriyGrosh <dmitriy.grosh@gmail.com>
2021-05-29 08:59:45 +0300

    добавил страницу объявления

---
+++ src/library/components/Search/Search.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { dataMessageMock } from '../../../__mocks__/dataMessageMock';
+import OrderMessage from '../OrderMessage/OrderMessage';
+
+const Search = () => <OrderMessage data={dataMessageMock} />;
+
+export default Search;
--- src/main/Routes.tsx
+++ src/main/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Authorization from 'pages/Authorization';
+import Orders from '../pages/Orders/Orders';
 
 const Routes = () => {
 	console.log('');
@@ -9,6 +10,7 @@ const Routes = () => {
 		<Switch>
 			<Route exact path="/" render={() => <p>TITLe</p>} />
 			<Route exact path="/login" component={Authorization} />
+			<Route path="/products" component={Orders} />
 		</Switch>
 	);
 };
