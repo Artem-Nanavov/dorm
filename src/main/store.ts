18978b34f4c57ece776cf598ce4c64c7e03e029e
artem_nanavov <artemnanavov@gmail.com>
2021-05-29 12:26:33 +0300

    init

---
+++ src/main/store.ts
@@ -0,0 +1,12 @@
+// import PostStore from '../../pages/vk/stores/vkStore';
+
+class RootStore {
+	// vkStore: PostStore;
+
+	constructor() {
+		// this.vkStore = new PostStore();
+		console.log('');
+	}
+}
+
+export default RootStore;
