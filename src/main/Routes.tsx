cbc9a6edc730410f09f67b260f610199d83cea94
DmitriyGrosh <dmitriy.grosh@gmail.com>
2021-05-29 10:05:04 +0300

    commit

--- src/main/Routes.tsx
+++ src/main/Routes.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Authorization from 'pages/Authorization';
+import Header from '../library/components/Header/Header';
 
 // import Login from "../pages/Authorization/Login/Login";
+
+const Routes = () => {
+	console.log('');
 
 	return (
 		<>
