b53c3d99b21eea33cb0b34f3c082e3c74b28352d
Lisa <elizaveta.okorakova.anatolyevna@gmail.com>
2021-05-30 14:46:27 +0300

    Исправлена страница разработки

--- src/library/components/PageDev/PageDev.tsx
+++ src/library/components/PageDev/PageDev.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import styles from './PageDev.scss';
 
+import pagedev from '../../../images/pagedev.jpeg';
+
 const PageDev = () => (
 	<div className={styles.PageDev}>
-		<div className={styles.square}>
-			<div className={styles.text}>Раздел находится в разработке</div>
+		<div className={styles.p}>
+			<p className={styles.text}>Раздел находится в разработке</p>
 		</div>
-		<div className={styles.photo} />
+		<div className={styles.photo}>
+			<img src={pagedev} alt="error" />
+		</div>
 	</div>
 );
 export default PageDev;
